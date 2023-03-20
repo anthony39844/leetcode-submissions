55c066f1a2f20b9a0bf745c3ff24ea71d314474a
Anthony Zheng <108604749+anthony39844@users.noreply.github.com>
2023-03-20 19:20:00 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42.1 MB (99.99%)

---
+++ solutions/0035-search-insert-position/solution.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int high = nums.length - 1;
+        int low = 0;
+        while ( high >= low){
+            int mid = low + (high - low) / 2;
+            if (target > nums[mid]){
+                low = mid + 1;
+            }else if (target < nums[mid]){
+                high = mid - 1;
+            }else{
+                return mid;
+            }
+        }
+        return low;
+        // int index = 0;
+        // while (high - low > 0){
+        //     int mid = low + (high - low) / 2; 
+        //     if (target == nums[mid]){ 
+        //         return mid;
+        //     }else if (target > nums[mid]){
+        //         low = mid + 1;
+        //     }else{
+        //         high = mid - 1; 
+        //     }
+        // }
+        // int count = 0;
+        // if (target > nums[nums.length - 1]){
+        //     return nums.length;
+        // }
+        // while (target > nums[count]){
+        //     count++;
+        //     index = count;
+        // }
+        // return index;
+        
+    }
+}
