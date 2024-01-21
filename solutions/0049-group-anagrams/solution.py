105bdefe27c4b890c0cbdcd73ee312a7b7dbeac7
Anthony Zheng <108604749+anthony39844@users.noreply.github.com>
2024-01-21 22:38:28 +0000

    Sync LeetCode submission Runtime - 77 ms (5.34%), Memory - 19.6 MB (100.00%)

--- solutions/0049-group-anagrams/solution.py
+++ solutions/0049-group-anagrams/solution.py
@@ -1,17 +1,14 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        anagram_map = {}
-        for s in strs:
-        # Sort the string and use it as a key in the hashmap
-            sorted_str = ''.join(sorted(s))
-
-        # If the sorted string is already in the hashmap, append the original string to its value
-            if sorted_str in anagram_map:
-                anagram_map[sorted_str].append(s)
-                print(anagram_map[sorted_str])
-            else:
-            # If the sorted string is not in the hashmap, create a new key-value pair
-                anagram_map[sorted_str] = [s]
-        grouped_anagrams = list(anagram_map.values())
-        return grouped_anagrams
-
+        map = {}
+        answer = []
+        for i in strs:
+            sort = ''.join(sorted(i))
+            if sort not in map:
+                map[sort] = []
+            map[sort].append(i)
+        
+        for val in map.values():
+            answer.append(val)
+        return answer
+            
