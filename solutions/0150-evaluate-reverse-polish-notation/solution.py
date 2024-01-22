cc7f4be0953d79a70d0e66989e2d3b0d899b3a8b
Anthony Zheng <108604749+anthony39844@users.noreply.github.com>
2024-01-22 19:07:19 +0000

    Sync LeetCode submission Runtime - 65 ms (5.28%), Memory - 17.2 MB (100.00%)

--- solutions/0150-evaluate-reverse-polish-notation/solution.py
+++ solutions/0150-evaluate-reverse-polish-notation/solution.py
@@ -1,24 +1,24 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-        stack = []
-        answer = 0
-        if len(tokens) == 1: # if the list is only a number just return the number
+        if len(tokens) == 1:
             return int(tokens[0])
-        for i in tokens: #for every value in the list 
-            if i != '+' and i != '*' and i != '/' and i != '-':
-                stack.append(i)
-                #if it is not a operator then append it to the stack
-            else:
-                x = int(stack.pop()) #else pop the last two elements and perform the operation
-                y = int(stack.pop())
-                if i == '+':
-                    answer = y + x
-                elif i == '-':
-                    answer = y - x
-                elif i == '/':
-                    answer = y / x
-                elif i == '*':
-                    answer = y * x
-                stack.append(answer) #append the new answer to the stack
-        return int(stack.pop())
 
+        s = []
+        ops = {'+' : 0, '-': 0, '*': 0, '/': 0}
+
+        for i in tokens:
+            if i not in ops:
+                s.append(int(i))
+            else:
+                x = s.pop()
+                y = s.pop()
+                if i == '+':
+                    s.append(x + y)
+                elif i == '-':
+                    s.append(y - x)
+                elif i == '*':
+                    s.append(x * y)
+                elif i == '/':
+                    s.append(int(y / x))
+
+        return s.pop()
