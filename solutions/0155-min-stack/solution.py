2f24ad6cda98f11e1a46cb3852520b0b99ba6ef6
Anthony Zheng <108604749+anthony39844@users.noreply.github.com>
2024-01-22 01:37:56 +0000

    Sync LeetCode submission Runtime - 46 ms (8.27%), Memory - 20.3 MB (100.00%)

--- solutions/0155-min-stack/solution.py
+++ solutions/0155-min-stack/solution.py
@@ -1,26 +1,30 @@
 class MinStack:
 
     def __init__(self):
-        self.stack = []
-        self.minstack = []
+        self.list = []
+        self.count = 0
+        self.minlist = []
 
     def push(self, val: int) -> None:
-        if not self.minstack or val <= self.minstack[-1]:
-            #if the minstack is empty or if the val is less than the most recent val in the min stack, then append the val to minstack
-            self.minstack.append(val)
-        self.stack.append(val) #append the val to the stack
-
+        if not self.minlist or val <= self.minlist[-1]:
+            self.minlist.append(val)
+        self.list.append(val)
+        self.count += 1
 
     def pop(self) -> None:
-        if self.minstack[-1] == self.stack[-1]: #if the last element in the minstack is the same as the element we are removing in stack then remove that val from the minstack as well
-            self.minstack.pop()
-        self.stack.pop()
+        if self.count > 0:
+            remove = self.list.pop(self.count - 1)
+            if remove == self.minlist[-1]:
+                self.minlist.pop()
+            self.count -= 1
 
     def top(self) -> int:
-        return self.stack[-1]
+        return self.list[self.count - 1]
 
     def getMin(self) -> int:
-        return self.minstack[-1] #the last element will always be the smallest element
+        if len(self.list) == 0:
+            return None
+        return self.minlist[-1]
 
 
 # Your MinStack object will be instantiated and called as such:
