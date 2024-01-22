97b143867a7ff99e07333cb303de1f41f18017bb
Anthony Zheng <108604749+anthony39844@users.noreply.github.com>
2024-01-22 01:56:34 +0000

    Sync LeetCode submission Runtime - 36 ms (0.99%), Memory - 16.6 MB (100.00%)

--- solutions/0021-merge-two-sorted-lists/solution.py
+++ solutions/0021-merge-two-sorted-lists/solution.py
@@ -5,23 +5,36 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = ListNode(-1) #make dummy node
-        current = dummy
+        curr1 = list1
+        curr2 = list2
 
-        while list1 and list2: #while both lists are not empty
-            if list1.val < list2.val: #point to list1 if list1 is less than
-               current.next = list1
-               list1 = list1.next #move list1 to the next node
-            else: 
-                current.next = list2 #point to list2 if list2 is less than
-                list2 = list2.next #move list2 to the next node
-            
-            current = current.next #move current node to the next node
-        
-        if list1 or list2:
-            current.next = list1 if list1 else list2
-        
-        return dummy.next
+        if not list1:
+            return list2
+        if not list2: 
+            return list1
 
-             
+        if list1.val <= list2.val:
+            curr = list1
+            curr1 = curr1.next
+        else:
+            curr = list2
+            curr2 = curr2.next
+        head = curr
+
+        while curr1 and curr2:
+            if curr1.val >= curr2.val:
+                curr.next = curr2
+                curr2 = curr2.next
+            else:
+                curr.next = curr1
+                curr1 = curr1.next
+            curr = curr.next
             
+        if curr1:
+            curr.next = curr1
+        if curr2:
+            curr.next = curr2
+
+        return head
+        
+
