2f6eec7c774fc0a9adbc3874bff2e51934cfc311
Anthony Zheng <108604749+anthony39844@users.noreply.github.com>
2024-01-23 22:31:00 +0000

    Sync LeetCode submission Runtime - 81 ms (6.61%), Memory - 16.5 MB (100.00%)

--- solutions/0036-valid-sudoku/solution.py
+++ solutions/0036-valid-sudoku/solution.py
@@ -1,35 +1,57 @@
+from collections import defaultdict
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        for i in range(9): #checks the rows
-            nums_dict = dict()
-            for counter in range(9):
-                if board[i][counter] != ".":
-                    if board[i][counter] in nums_dict:
+        rowdict = defaultdict(set)
+        coldict = defaultdict(set)
+        boxdict = defaultdict(set)
+
+        for i in range(9):
+            for j in range(9):
+                if board[i][j] != '.':
+                    box = i//3 * 3 + j//3
+                    if board[i][j] in rowdict[i] or board[i][j] in coldict[j] or board[i][j] in boxdict[box]:
                         return False
                     else:
-                        nums_dict[board[i][counter]] = 1
-        for i in range(9): #checks the columns
-            nums_dict = dict()
-            for counter in range(9):
-                if board[counter][i] != ".":
-                    if board[counter][i] in nums_dict:
-                        return False
-                    else:
-                        nums_dict[board[counter][i]] = 1
-        for j in range(3):
-            j *= 3
-            for row in range(3): #checking each box
-                nums_dict = dict()
-                row *= 3
-                for i in range(3):
-                    for col in range(3):
-                        if board[row + i][col + j] != ".":
-                            if board[row + i][col + j] in nums_dict:
-                                return False
-                            else:
-                                nums_dict[board[row + i][col + j]] = 1
-        
-
-
+                        rowdict[i].add(board[i][j])
+                        coldict[j].add(board[i][j])
+                        boxdict[box].add(board[i][j])
         return True
+
+        # s = set()
+        # for i in range(9):
+        #     for j in range(9):
+        #         if board[i][j] != '.':
+                    
+        #             if board[i][j] not in s:
+        #                 s.add(board[i][j])
+        #             else:
+        #                 return False
+        #     s = set()
+        # s = set()
+        # for i in range(9):
+        #     for j in range(9):
+        #         if board[j][i] != '.':
+                    
+        #             if board[j][i] not in s:
+        #                 s.add(board[j][i])
+        #             else:
+        #                 return False
+        #     s = set()   
+        # s = set()
+        # for i in range(0, 9, 3):
+        #     for j in range(0, 9, 3):
+        #         for m in range(i, i + 3):
+        #             for n in range(j, j + 3):
+        #                 if board[m][n] != '.':
+        #                     if board[m][n] not in s:
+        #                         s.add(board[m][n])
+        #                     else:
+        #                         return False
+
+        #         s=set()
+        # return True
         
+        
+
+
+
