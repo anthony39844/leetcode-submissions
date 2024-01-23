74d9c5269750ebf01981e575f818e3795a54c803
Anthony Zheng <108604749+anthony39844@users.noreply.github.com>
2024-01-23 01:27:45 +0000

    Sync LeetCode submission Runtime - 1559 ms (5.37%), Memory - 16.9 MB (100.00%)

--- solutions/0242-valid-anagram/solution.py
+++ solutions/0242-valid-anagram/solution.py
@@ -1,9 +1,18 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        s_dict = {}
-        t_dict = {}
-        for x in s:
-            s_dict[x] = s.count(x)
-        for x in t:
-            t_dict[x] = t.count(x)
-        return s_dict == t_dict
+        if (len(s) != len(t)):
+            return False
+
+        letterCount = {}
+        for char in s:
+            if char not in letterCount:
+                letterCount[char] = 1
+            else:
+                letterCount[char] += 1
+
+        for char in t:
+            if char not in letterCount or (t.count(char) != letterCount[char]) :
+                return False
+
+        return True
+        
