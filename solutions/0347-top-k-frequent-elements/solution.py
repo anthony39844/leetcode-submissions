65b1ca70ef73f5eb77a0be067d554fb880b6f2d3
Anthony Zheng <108604749+anthony39844@users.noreply.github.com>
2024-01-23 04:07:09 +0000

    Sync LeetCode submission Runtime - 78 ms (5.11%), Memory - 21 MB (100.00%)

--- solutions/0347-top-k-frequent-elements/solution.py
+++ solutions/0347-top-k-frequent-elements/solution.py
@@ -1,8 +1,13 @@
-from collections import Counter
-
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        my_counter = Counter(nums)
-        nums_list = list([value for value, count in my_counter.most_common(k)])
-        print(nums_list)
-        return nums_list
+        d = dict()
+        out = []
+        for i in nums:
+            d[i] = d.get(i, 0) + 1
+        d = sorted(d.items(), key=lambda item: item[1], reverse=True)
+        for val in d:
+            out.append(val[0])
+            if len(out) == k:
+                return out
+
+            
