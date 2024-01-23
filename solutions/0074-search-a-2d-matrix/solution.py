cf1ca19ee41fbf323347d33130ac73e20b10e714
Anthony Zheng <108604749+anthony39844@users.noreply.github.com>
2024-01-23 01:44:48 +0000

    Sync LeetCode submission Runtime - 38 ms (0.10%), Memory - 17 MB (100.00%)

--- solutions/0074-search-a-2d-matrix/solution.py
+++ solutions/0074-search-a-2d-matrix/solution.py
@@ -1,18 +1,12 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        i = 0
-        j = len(matrix[0]) - 1
-        if j == 0 and len(matrix) == 1:
-            return matrix[0][0] == target
-
-        while i < len(matrix):
-            if matrix[i][j] == target:
+        row = 0
+        col = len(matrix[row]) - 1
+        while row < len(matrix) and col >= 0:
+            if matrix[row][col] < target:
+                row += 1
+            elif matrix[row][col] > target:
+                col -= 1
+            else:
                 return True
-            elif matrix[i][j] > target:
-                j -= 1
-                if j < 0:
-                    return False
-            elif matrix[i][j] < target:
-                i += 1
         return False
-        
