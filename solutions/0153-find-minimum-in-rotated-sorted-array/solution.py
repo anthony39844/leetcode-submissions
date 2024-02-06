14aaae9aecc3e495105e532448297f01d475c690
Anthony Zheng <108604749+anthony39844@users.noreply.github.com>
2024-02-06 01:16:05 +0000

    Sync LeetCode submission Runtime - 44 ms (0.15%), Memory - 16.9 MB (100.00%)

--- solutions/0153-find-minimum-in-rotated-sorted-array/solution.py
+++ solutions/0153-find-minimum-in-rotated-sorted-array/solution.py
@@ -1,39 +1,24 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-            # 0 1 2 3 4 5 6 
-            # 1 2 3 4 5 6 0 
-            # 2 3 4 5 6 0 1
-            # 3 4 5 6 0 1 2
-            # 4 5 6 0 1 2 3
-            # 5 6 0 1 2 3 4
-            # 6 0 1 2 3 4 5
         left, right = 0, len(nums) - 1
-
-        if len(nums) == 1:
-            return nums[0]
-            
-        if nums[0] < nums[right]: #means it is not rotated
-            return nums[0]
-        else:
-            while left < right:
-                mid = (left + right) // 2
-                if left + 1 == right:
-                    if nums[left] > nums[right]:
-                        return nums[right]
-                    else:
-                        return nums[left]
-                elif nums[mid] < nums[right]:
-                    right = mid
-                elif nums[mid] > nums[right]:
-                    left = mid
+        while left < right:
+            mid = (left + right) // 2
+            if nums[mid] > nums[right]:
+                left = mid + 1
+            else:
+                right = mid 
+        return nums[left]
 
 
-
-
-
-
-            
-        
-
-            
-
+        # if len(nums) == 1:
+        #     return nums[0]
+        # for i in range(1, len(nums)):
+        #     if nums[i] < nums[i - 1]:
+        #         break
+        # if i == len(nums) - 1:
+        #     if nums[i] > nums[0]:
+        #         return nums[0]
+        #     else:
+        #         return nums[i]
+        # else:
+        #     return nums[i]
