2a586f59dba32a601f9363181807cc9e6216ae0c
Anthony Zheng <108604749+anthony39844@users.noreply.github.com>
2024-09-02 07:23:15 +0000

    Sync LeetCode submission Runtime - 190 ms (4.22%), Memory - 18.2 MB (100.00%)

--- solutions/0792-binary-search/solution.py
+++ solutions/0792-binary-search/solution.py
@@ -1,13 +1,15 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        right = len(nums) - 1
-        left = 0
-        while left <= right:
-            mid = right + left // 2
-            if nums[mid] < target:
-                left = mid + 1
-            elif nums[mid] > target:
-                right = mid - 1
+        l = 0
+        r = len(nums) - 1
+
+        while l <= r:
+            mid = (l + r) // 2
+            print(nums[mid])
+            if nums[mid] > target:
+                r = mid - 1
+            elif nums[mid] < target:
+                l = mid + 1
             elif nums[mid] == target:
                 return mid
         return -1
