353f0c6eacf74c7c7f27971d224395cf5f89aaa6
Anthony Zheng <108604749+anthony39844@users.noreply.github.com>
2024-09-02 02:41:52 +0000

    Sync LeetCode submission Runtime - 85 ms (6.61%), Memory - 16.5 MB (100.00%)

--- solutions/0036-valid-sudoku/solution.py
+++ solutions/0036-valid-sudoku/solution.py
@@ -1,57 +1,38 @@
-from collections import defaultdict
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        rowdict = defaultdict(set)
-        coldict = defaultdict(set)
-        boxdict = defaultdict(set)
-
-        for i in range(9):
-            for j in range(9):
-                if board[i][j] != '.':
-                    box = i//3 * 3 + j//3
-                    if board[i][j] in rowdict[i] or board[i][j] in coldict[j] or board[i][j] in boxdict[box]:
+        s = set()
+        for i in board:
+            s.clear()
+            for j in i:
+                if j != ".":
+                    if j in s:
                         return False
-                    else:
-                        rowdict[i].add(board[i][j])
-                        coldict[j].add(board[i][j])
-                        boxdict[box].add(board[i][j])
+                    s.add(j)
+
+        s.clear()
+        for i in range(len(board)):
+            s.clear()
+            for j in range(len(board)):
+                if board[j][i] != ".":
+                    if board[j][i] in s:
+                        return False
+                    s.add(board[j][i])
+
+    
+        s.clear()
+        for i in range(0, 9, 3):
+            for j in range(0, 9, 3):
+                s.clear()
+                for k in range(3):
+                    for l in range(3):
+                        cur = board[i + k][j + l]
+                        if cur != ".":    
+                            if cur in s:
+                                return False
+                            s.add(cur)
+        
         return True
-
-        # s = set()
-        # for i in range(9):
-        #     for j in range(9):
-        #         if board[i][j] != '.':
-                    
-        #             if board[i][j] not in s:
-        #                 s.add(board[i][j])
-        #             else:
-        #                 return False
-        #     s = set()
-        # s = set()
-        # for i in range(9):
-        #     for j in range(9):
-        #         if board[j][i] != '.':
-                    
-        #             if board[j][i] not in s:
-        #                 s.add(board[j][i])
-        #             else:
-        #                 return False
-        #     s = set()   
-        # s = set()
-        # for i in range(0, 9, 3):
-        #     for j in range(0, 9, 3):
-        #         for m in range(i, i + 3):
-        #             for n in range(j, j + 3):
-        #                 if board[m][n] != '.':
-        #                     if board[m][n] not in s:
-        #                         s.add(board[m][n])
-        #                     else:
-        #                         return False
-
-        #         s=set()
-        # return True
-        
+                
         
 
-
-
+        
