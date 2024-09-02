3eb63c9675c9a706d6bdd674aea17884106d1b5d
Anthony Zheng <108604749+anthony39844@users.noreply.github.com>
2024-09-02 03:28:31 +0000

    Sync LeetCode submission Runtime - 50 ms (5.23%), Memory - 17.1 MB (100.00%)

--- solutions/0125-valid-palindrome/solution.py
+++ solutions/0125-valid-palindrome/solution.py
@@ -1,17 +1,19 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
         left = 0
         right = len(s) - 1
-        s = s.lower()
-        while left <= right:
+        while left < right:
             if not s[left].isalnum():
                 left += 1
-            elif not s[right].isalnum():
+                continue
+            if not s[right].isalnum():
                 right -= 1
-            else:
-                if s[left] != s[right]:
-                    return False
-                left += 1
-                right -= 1
-
+                continue
+            if s[right] != s[left]:
+                return False
+            left += 1
+            right -= 1
         return True
+            
+
