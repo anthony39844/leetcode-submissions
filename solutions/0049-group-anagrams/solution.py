78cd54da653e599bb0b70a9378be96dad44edbf5
Anthony Zheng <108604749+anthony39844@users.noreply.github.com>
2024-09-04 01:26:13 +0000

    Sync LeetCode submission Runtime - 81 ms (5.34%), Memory - 19.4 MB (100.00%)

--- solutions/0049-group-anagrams/solution.py
+++ solutions/0049-group-anagrams/solution.py
@@ -1,14 +1,15 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         d = {}
-        out = []
         for i in strs:
-            s = "".join(sorted(i))
-            if s not in d:
-                d[s] = [i]
-            else:
+            s = ''.join(sorted(i))
+            if s in d:
                 d[s].append(i)
+            else:
+                d[s] = [i]
+
+        out = []
         for i in d:
-            out.append(d[i])
+           out.append(d[i]) 
         return out
 
