6031fc5a478804d7b5bc8482a507e0b261de2090
Anthony Zheng <108604749+anthony39844@users.noreply.github.com>
2024-09-06 20:33:03 +0000

    Sync LeetCode submission Runtime - 903 ms (5.06%), Memory - 30.8 MB (28.85%)

--- solutions/0739-daily-temperatures/solution.py
+++ solutions/0739-daily-temperatures/solution.py
@@ -1,37 +1,12 @@
 class Solution:
-    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:  
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         out = [0] * len(temperatures)
         s = []
+
         for i in range(len(temperatures)):
-            while s and temperatures[i] > temperatures[s[-1]]:
-                index = s.pop()
-                out[index] = i - index
-            s.append(i)
+            while s and temperatures[i] > s[-1][0]:
+                out[s[-1][1]] = i - s[-1][1]
+                s.pop()
+            s.append((temperatures[i], i))
+
         return out
-
-        # out = []
-        # s = []
-        # for i in range(len(temperatures) - 1):
-        #     s.append(temperatures[i + 1])
-        #     if temperatures[i] < temperatures[i + 1]:
-        #         out.append(len(s))
-        #         s = []
-        #     else:
-        #         j = i + 1
-        #         while temperatures[j] <= temperatures[i]:
-        #             j += 1
-        #             if j >= len(temperatures):
-        #                 break
-        #             s.append(temperatures[j])
-        #         if j >= len(temperatures):
-        #             out.append(0)
-        #         else:
-        #             out.append(len(s))
-        #         s = []
-        # out.append(0)
-        # return out
-
-
-
-
-        
