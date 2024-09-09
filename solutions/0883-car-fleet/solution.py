7047a339d3bcc3ed896d286fd5ce920f47223ed3
Anthony Zheng <108604749+anthony39844@users.noreply.github.com>
2024-09-09 05:19:43 +0000

    Sync LeetCode submission Runtime - 739 ms (5.04%), Memory - 36.8 MB (99.01%)

--- solutions/0883-car-fleet/solution.py
+++ solutions/0883-car-fleet/solution.py
@@ -1,27 +1,12 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        if len(position) == 1:
-            return 1
-        
         stack = []
-        pairs = []
-
-        for i in range(len(position)): #we are making a list of tuples of the position and speed for each car
-            pairs.append([position[i], speed[i]])
-
-        for i in sorted(pairs)[::-1]: #loop through that list backwards
-            if not stack: 
-                stack.append(i)
-            else:
-                curr_time = (target - i[0]) / i[1]
-                time = (target - stack[-1][0]) / stack[-1][1]
-
-                if curr_time > time: #if the time for the current car is greater than the car in the stack
-                                     #that means it wont become a fleet so we append the car to the stack
-                    stack.append(i)
-        # the idea is that we will use how long it takes for each car to reach the target by dividing the 
-        # distance it is away from the target by its speed. If the time it needs is greater than the car closest
-        # to the target, then it will become its own fleet of cars. If it is less than, then it will join the
-        # a fleet so we dont append that car to the stack. The length of the stack at the end is the number of fleets
+        pair = [[p,s] for p, s in zip(position, speed)]
+        
+        for p, s in sorted(pair)[::-1]:
+            t = (target - p) / s
+            stack.append(t)
+            if len(stack) >= 2 and stack[-1] <= stack[-2]:
+                    stack.pop()
 
         return len(stack)
