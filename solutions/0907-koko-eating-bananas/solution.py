bf48a660875a11b4cc58b3d05c6fbdea24835278
Anthony Zheng <108604749+anthony39844@users.noreply.github.com>
2024-10-25 07:33:34 +0000

    Sync LeetCode submission Runtime - 147 ms (98.10%), Memory - 17.8 MB (100.00%)

--- solutions/0907-koko-eating-bananas/solution.py
+++ solutions/0907-koko-eating-bananas/solution.py
@@ -1,21 +1,17 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        
-        if h == len(piles):
-            return max(piles)
-        right, left = max(piles), 1
-        mid = 0
-        while left < right:
-            hour = 0
-            mid = left + (right - left) // 2
+        l = 1
+        r = max(piles)
+        res = r
+        while l <= r:
+            count = 0
+            mid = (l + r) // 2
             for i in piles:
-                hour += ceil(i / mid)
-            if hour <= h:
-                right = mid #there is a smaller k
-            elif hour > h:
-                left = mid + 1
-        return left 
+                count += math.ceil(i/mid)
+            if count <= h:
+                res = min(mid, res)
+                r = mid - 1
+            elif count > h:
+                l = mid + 1
+        return res
 
-             
-
-       
