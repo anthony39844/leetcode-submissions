8ea3edbff086ef995a20abd3dc9ecbf6d7129fcf
Anthony Zheng <108604749+anthony39844@users.noreply.github.com>
2025-02-10 05:29:07 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.6 MB (100.00%)

--- solutions/0019-remove-nth-node-from-end-of-list/solution.py
+++ solutions/0019-remove-nth-node-from-end-of-list/solution.py
@@ -5,38 +5,21 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        if not head.next: 
-            return None
+        dummy = ListNode()
+        dummy.next = head
+        cur = head
 
-        current = head #reverse list
-        rev = None
-        while current:
-            next_node = current.next
-            current.next = rev
-            rev = current
-            current = next_node #rev will be the head of the reversed list
+        for i in range(n): #move up n spots
+            cur = cur.next
 
-        head2 = rev
-        curr = rev
-
-        for i in range(n - 1):
-            curr = curr.next #loop to the correct node
-
-        if n == 1:  #if n is 1 or we are removing the first node of the reversed list just move up rev
-            rev = rev.next
-            head2 = rev
-        else: 
-            for i in range(n - 2): #move rev to the node before the one we are moving
-                rev = rev.next
-            next = curr.next #else we get the node after the one we are removing 
-            rev.next = next #and set the node after rev to next
-
-        current = head2 #reverse the list back
-        prev = None
-        while current:
-            next_node = current.next
-            current.next = prev
-            prev = current
-            current = next_node
-        return prev
+        prev = dummy
+        while cur: #move both up until cur is at the end, this will leave prev at 1 spot before the nth node from the end
+            prev = prev.next
+            cur = cur.next
+        
+        prev.next = prev.next.next 
+        # node past nth node from end: will set it to that node
+        # no past nth node from end: will set to None
+        # removed only node list: prev will still be dummy so next.next will go to None so dummy.next will be None
+        return dummy.next
 
