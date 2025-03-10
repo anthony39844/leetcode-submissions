85abc79ea1b42ab0f3b9b91fd06e5b951c9d4864
Anthony Zheng <108604749+anthony39844@users.noreply.github.com>
2025-03-10 23:05:36 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.2 MB (99.98%)

--- solutions/0001-two-sum/solution.py
+++ solutions/0001-two-sum/solution.py
@@ -2,7 +2,7 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         d = {}
         for i in range(len(nums)):
+            x = target - nums[i]
             if nums[i] in d:
-                return [d[nums[i]], i]
-            else:
-                d[target - nums[i]] = i
+                return [i, d[nums[i]]]
+            d[x] = i
