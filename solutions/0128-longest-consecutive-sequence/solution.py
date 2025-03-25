eb02244a2551987b3ce0a0b5ca8af6f1bbea4309
Anthony Zheng <108604749+anthony39844@users.noreply.github.com>
2025-03-25 23:27:08 +0000

    Sync LeetCode submission Runtime - 75 ms (13.67%), Memory - 34.2 MB (92.02%)

--- solutions/0128-longest-consecutive-sequence/solution.py
+++ solutions/0128-longest-consecutive-sequence/solution.py
@@ -3,13 +3,12 @@ class Solution:
         m = 0
         s = set(nums)
         for i in s:
-            num = i
             count = 1
-            if num - 1 not in s:
-                while num + 1 in s:
+            if i + 1 not in s:
+                while i - 1 in s:
                     count += 1
-                    num += 1
-                m = max(m, count)
+                    i -=1
+            m = max(count, m)
         return m
             
             
