e8d998f20291a2dda12dee5b62395027dda69297
Anthony Zheng <108604749+anthony39844@users.noreply.github.com>
2025-03-26 05:08:16 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 18.1 MB (100.00%)

--- solutions/0153-find-minimum-in-rotated-sorted-array/solution.py
+++ solutions/0153-find-minimum-in-rotated-sorted-array/solution.py
@@ -1,16 +1,26 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
+        # l, r = 0, len(nums) - 1
+\
+        '''
+        1 2 3 4 5 
+        5 1 2 3 4
+        4 5 1 2 3
+        3 4 5 1 2 
+        2 3 4 5 1
+        '''
+
         l, r = 0, len(nums) - 1
 
-        cur = nums[0]
         while l <= r:
-            if nums[l] < nums[r]:
-                return min(cur, nums[l])
+            m = (l + r) // 2
+            print(m)
+            if nums[m] < nums[r] and m > 0 and nums[m] < nums[m - 1]: # we know the right side is increasing and m is smallest 
 
-            mid = (l + r) // 2 
-            cur = min(cur, nums[mid])
-            if nums[mid] >= nums[l]:
-                l = mid + 1
-            else:
-                r = mid - 1
-        return cur
+                return nums[m]
+            elif nums[m] < nums[r]: #right side increasing, but pivot is still on the left
+                r = m - 1
+            else: # we know the pivot is on the left
+                l = m + 1
+
+        return nums[m]
