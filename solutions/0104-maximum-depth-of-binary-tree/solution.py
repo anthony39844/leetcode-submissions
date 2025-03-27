7bfc8c7f40f75b6ef35df21c2d039c1ed30d3c42
Anthony Zheng <108604749+anthony39844@users.noreply.github.com>
2025-03-27 19:19:25 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 18.9 MB (100.00%)

--- solutions/0104-maximum-depth-of-binary-tree/solution.py
+++ solutions/0104-maximum-depth-of-binary-tree/solution.py
@@ -7,9 +7,12 @@
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         
-        if not root:
-            return 0
-        else:
-            l = self.maxDepth(root.left)
-            r = self.maxDepth(root.right)
-            return max(l, r) + 1
+        def search(node):
+            if not node:
+                return 0
+            maxL = search(node.left) + 1
+            maxR = search(node.right) + 1
+
+            return max(maxL, maxR)
+
+        return search(root)
