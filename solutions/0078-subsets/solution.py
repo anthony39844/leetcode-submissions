339cb99e3208c0be46ca997f88b7dc26144ed794
Anthony Zheng <108604749+anthony39844@users.noreply.github.com>
2025-07-31 02:45:09 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.8 MB (100.00%)

--- solutions/0078-subsets/solution.py
+++ solutions/0078-subsets/solution.py
@@ -1,13 +1,19 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         out = []
-        def backtrack(i, curr):
-            out.append(curr.copy())
-            for i in range(i, len(nums)):
-                curr.append(nums[i])
-                backtrack(i + 1, curr)
-                curr.pop()
-        
-        backtrack(0, [])
-        return out
+        cur = []
+        def backtrack(i):
+            if i >= len(nums):
+                out.append(cur.copy())
+                return
 
+            cur.append(nums[i])
+            backtrack(i + 1)
+
+            cur.pop()
+            backtrack(i + 1)
+
+        backtrack(0)
+
+        return out
+        
