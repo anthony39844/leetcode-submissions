79f724248cd3a8c181ef33601f14cd2cdc24d0f5
Anthony Zheng <108604749+anthony39844@users.noreply.github.com>
2025-08-05 03:07:51 +0000

    Sync LeetCode submission Runtime - 11 ms (76.24%), Memory - 18 MB (100.00%)

--- solutions/0242-valid-anagram/solution.py
+++ solutions/0242-valid-anagram/solution.py
@@ -1,14 +1,16 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
         d = {}
-        d2 = {}
-
+        e = {}
         for i in s:
             d[i] = d.get(i, 0) + 1
-        
+
         for i in t:
-            d2[i] = d2.get(i, 0) + 1
+            e[i] = e.get(i, 0) + 1
         
-        if d == d2:
+        if e == d:
             return True
-        return False
+        else:
+            return False
