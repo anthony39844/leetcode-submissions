d848fed9a42caa6c843d255db2740ae7c40e7034
Anthony Zheng <108604749+anthony39844@users.noreply.github.com>
2025-08-31 22:48:38 +0000

    Sync LeetCode submission Runtime - 3 ms (98.98%), Memory - 19.1 MB (99.96%)

--- solutions/0125-valid-palindrome/solution.py
+++ solutions/0125-valid-palindrome/solution.py
@@ -1,16 +1,12 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s = s.lower()
-
         st = []
-        st2 = []
+        s = s.lower()
         for i in s:
             if i.isalnum():
                 st.append(i)
-    
-        for i in range(len(s) - 1, -1, -1):
-            if s[i].isalnum():
-                st2.append(s[i])
-        
-        return st == st2
 
+        if st == st[::-1]:
+            return True
+        else:
+            return False
