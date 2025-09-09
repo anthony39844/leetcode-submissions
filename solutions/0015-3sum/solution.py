5c63b61c76102316a2ae2ce7d0c802f104e1d002
Anthony Zheng <108604749+anthony39844@users.noreply.github.com>
2025-09-09 23:49:13 +0000

    Sync LeetCode submission Runtime - 455 ms (96.07%), Memory - 20.7 MB (99.95%)

--- solutions/0015-3sum/solution.py
+++ solutions/0015-3sum/solution.py
@@ -3,22 +3,25 @@ class Solution:
         nums.sort()
         out = []
         for i in range(len(nums)):
-            if i > 0 and nums[i] == nums[i - 1]:
-                continue
             if nums[i] > 0:
                 break
-            j, k = i + 1, len(nums) - 1
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            k = len(nums) - 1
+            j = i + 1
             while j < k:
-                tot = nums[i] + nums[j] + nums[k]
-                if tot == 0:
-                    out.append((nums[i], nums[j], nums[k]))
-                    j += 1
-                elif tot < 0:
+                x = nums[i] + nums[j] + nums[k]
+                if x == 0:
+                    out.append([nums[i], nums[j], nums[k]])
+                    k -= 1
+                    while j < k and nums[j] == nums[j - 1]:
+                        j += 1
+                    while j < k and nums[k] == nums[k + 1]:
+                        k -= 1
+                elif x < 0:
                     j += 1
                 else:
                     k -= 1
-        
-        s = list(set(out))
 
-        return s
+        return out
 
