d42678fd0a015f52ad69b1abf00595497e8a8970
Anthony Zheng <108604749+anthony39844@users.noreply.github.com>
2025-10-27 18:39:28 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.8 MB (100.00%)

--- solutions/0013-roman-to-integer/solution.py
+++ solutions/0013-roman-to-integer/solution.py
@@ -1,16 +1,16 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        d = {"I" : 1, "V" : 5, "X" : 10, "L" : 50, "C" : 100, "D" : 500, "M" : 1000}
-        count = d[s[0]]
-        for i in range(1, len(s)):
-            print(d[s[i]], d[s[i - 1]])
-            if d[s[i]] > d[s[i - 1]]:
-                print("inside if")
-                count -= 2 * d[s[i - 1]]
-                count += d[s[i]]
+
+        mapping = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        out = 0
+        i = 0
+        while i < len(s):
+            if i + 1 < len(s) and mapping[s[i]] < mapping[s[i + 1]]:
+                out += (mapping[s[i + 1]] - mapping[s[i]])
+                i += 2
             else:
-                count += d[s[i]]
-            print(count)
-            
-        return count
+                out += mapping[s[i]]
+                i += 1
+        
+        return out
             
