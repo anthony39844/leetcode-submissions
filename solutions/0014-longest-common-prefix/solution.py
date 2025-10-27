f095f03c15adf7b73a51ea84b4931130422c0be3
Anthony Zheng <108604749+anthony39844@users.noreply.github.com>
2025-10-27 18:42:57 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.8 MB (100.00%)

--- solutions/0014-longest-common-prefix/solution.py
+++ solutions/0014-longest-common-prefix/solution.py
@@ -1,12 +1,13 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        out = ""
+ 
+        out = []
         shortest = min(strs, key=len)
         for i in range(len(shortest)):
             for j in range(len(strs)):
                 if shortest[i] == strs[j][i]:
                     continue
                 else:
-                    return out
-            out += shortest[i]
-        return out
+                    return "".join(out)
+            out.append(shortest[i])
+        return "".join(out)
