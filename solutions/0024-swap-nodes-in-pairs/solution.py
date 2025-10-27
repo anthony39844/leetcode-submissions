b82809a53a03900f1ec2bc852c2f38f320b0294c
Anthony Zheng <108604749+anthony39844@users.noreply.github.com>
2025-10-27 19:03:27 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.7 MB (100.00%)

--- solutions/0024-swap-nodes-in-pairs/solution.py
+++ solutions/0024-swap-nodes-in-pairs/solution.py
@@ -5,20 +5,24 @@
 #         self.next = next
 class Solution:
     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        '''
+        time = o(n)
+        space = o(1)
+        '''
         if not head:
             return None
         if not head.next and head: 
             return head
+            
         dummy = ListNode(0, head)
         prev = dummy
         first = head 
         second = first.next 
 
         while second: 
-            if prev:
-                prev.next = second 
-                first.next = second.next 
-                second.next = first 
+            prev.next = second 
+            first.next = second.next 
+            second.next = first 
 
             prev = first 
             first = first.next 
