0be3eb12b051ed7f053db81dc4847c876b482d96
Anthony Zheng <108604749+anthony39844@users.noreply.github.com>
2025-10-28 23:59:43 +0000

    Sync LeetCode submission Runtime - 3 ms (95.59%), Memory - 17.8 MB (100.00%)

--- solutions/0039-combination-sum/solution.py
+++ solutions/0039-combination-sum/solution.py
@@ -1,16 +1,25 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        '''
+        time = 
+        space = 
+        '''
+
         out = []
+        arr = []
 
-        def backtrack(index, subset):
-            if sum(subset) == target:
-                out.append(subset.copy())
-            elif sum(subset) < target:
-                for i in range(index, len(candidates)):
-                    subset.append(candidates[i]) 
-                    backtrack(i, subset)
-                    subset.pop()
+        def backtrack(arr, x, total):
+            if total == target:
+                out.append(arr.copy())
+                return
+            if total > target:
+                return
 
+            for i in range(x, len(candidates)):
+                arr.append(candidates[i])
+                backtrack(arr, i, total + candidates[i])
+                arr.pop()
 
-        backtrack(0, [])
+        backtrack([], 0, 0)
+
         return out
