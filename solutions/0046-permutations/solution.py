13b54c7ac5bc1042c56c83193822b034ad2648c2
Anthony Zheng <108604749+anthony39844@users.noreply.github.com>
2025-11-03 01:02:26 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.9 MB (100.00%)

--- solutions/0046-permutations/solution.py
+++ solutions/0046-permutations/solution.py
@@ -1,19 +1,22 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         '''
-        time = o()
+        time = o(n! * n^2)
         space = o(n)
         '''
         out = []
         used = [False] * len(nums)
         def backtrack(arr):
             if len(arr) == len(nums):
-                out.append(arr[:])
+                out.append(arr.copy())
                 return
+            
             for i in range(len(nums)):
                 if not used[i]:
                     used[i] = True
-                    backtrack(arr + [nums[i]])
+                    arr.append(nums[i])
+                    backtrack(arr)
+                    arr.pop()
                     used[i] = False
 
         backtrack([])
