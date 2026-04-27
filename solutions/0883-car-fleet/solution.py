1bac3883ae86f521e3fbf54d55d21586769f6cd7
Anthony Zheng <108604749+anthony39844@users.noreply.github.com>
2026-04-27 03:40:34 +0000

    Sync LeetCode submission Runtime - 201 ms (32.20%), Memory - 44.1 MB (10.79%)

--- solutions/0883-car-fleet/solution.py
+++ solutions/0883-car-fleet/solution.py
@@ -1,18 +1,15 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        s = []
-        p = []
-        for i in range(len(position)):
-            p.append((position[i], speed[i]))
-        p = sorted(p)
+        p, s = zip(*sorted(zip(position, speed), reverse=True))
+        p, s = list(p), list(s)
+        st = []
+        for i, x in enumerate(p):
+            t = ((target - x) / s[i])
+            if st and st[-1] >= t:
+                continue
+            st.append(t)
 
-        for i in range(len(p) - 1, -1, -1):
-            x, sp = p[i]
-            time = (target - x) / sp
-            if s:
-                if time > s[-1]:
-                    s.append(time)
-            else:
-                s.append(time)
-            
-        return len(s)
+        return len(st)
+
+
+
