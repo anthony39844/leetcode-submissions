0046d9eed0305e280531ddae3511ee6225ddd3d3
Anthony Zheng <108604749+anthony39844@users.noreply.github.com>
2026-05-04 23:27:41 -0500

    Delete solutions directory

--- solutions/0058-length-of-last-word/solution.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def lengthOfLastWord(self, s: str) -> int:
-        l = s.split(" ")
-        for i in l[::-1]:
-            if i.isalnum():
-                return len(i) 
-
--- solutions/0319-bulb-switcher/solution.java
+++
@@ -1,7 +0,0 @@
-class Solution {
-    public int bulbSwitch(int n) {
-        double root = Math.sqrt(n);
-        int count = (int) Math.floor(root);
-        return count;
-    }
-}
