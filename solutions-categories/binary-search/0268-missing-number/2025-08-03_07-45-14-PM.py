fc3dffa7f39b036e6e80124b5b13e019e340403c
Anthony Zheng <108604749+anthony39844@users.noreply.github.com>
2026-05-06 00:31:11 -0500

    Delete solutions-categories directory

--- solutions-categories/binary-search/0268-missing-number/2025-08-03_07-45-14-PM.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def missingNumber(self, nums: List[int]) -> int:
-        tot = 0
-        for i in range(1, len(nums) + 1):
-            tot += i
-        return tot - sum(nums)
-        
